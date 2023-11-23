7b9909cc0cdee1bf154326fef08a9645aebf51d5
manighahrmani <mashapicasso@gmail.com>
2023-11-23 14:12:44 +0000

    Update lecture until section 3

--- lect14.py
+++ lect14.py
@@ -1,8 +1,21 @@
 def readFile(filename):
     try:
-        with open(filename, 'r') as file:
-            content = file.read()
-            return content
+        inFile = open(filename, 'r')
+        content = inFile.read()
+        inFile.close()
+        return content
     except FileNotFoundError:
         print("File not found")
         return None
+    except PermissionError:
+        print("Permission denied")
+        return None
+
+
+def getChoice():
+    choice = input("Enter a number: ")
+    while not choice.isdigit():
+        print("That's not a number!")
+        choice = input("Enter a number: ")
+    choiceAsInt = int(choice)  # Can raise a `ValueError`
+    return choiceAsInt
