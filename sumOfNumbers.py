015b5e16046f0875682cbae8d2cee4741cbb305c
manighahrmani <mashapicasso@gmail.com>
2023-11-26 01:25:06 +0000

    Update print statement to clarify input requirement

--- sumOfNumbers.py
+++ sumOfNumbers.py
@@ -23,7 +23,7 @@ def main():
             numbers.append(number)
         else:
             break
-    print("Enter the total number of numbers in the list.")
+    print("To calculate the total, we need to know how many numbers are in the list.")
     count = getNumber()
     total = sumOfList(numbers, count)
     print("The sum of the numbers in the list is", total)
